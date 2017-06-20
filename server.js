60031b27ad65274def8ebcf7a5fc1e8f570334c3
Ben Neal <bengneal3@gmail.com>
2017-06-20 15:56:27 -0700

    Added pdf creation and mailing functionality

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 
 //PDF DEPENDENCIES
 var pdfkit = require('pdfkit');
+var fs = require('fs');
+
+//MAIL DEPENDENCIES
+var nodemailer = require('nodemailer');
 
 app.use(bodyParser.urlencoded({extended: false}));
 //app.use(express.static(__dirname + '/public'));
@@ -19,10 +23,25 @@ app.get('/', function (req, res){
 
 app.post('/generate', function (req, res){
   console.log('Generate route hit...');
-  console.log(req.body.name);
-  console.log(req.body.example);
-  console.log(req.body.email);
+
+  var contents = req.body;
+  generatePDF(contents);
+
   res.send('<h1>Generating resume!</h1>');
+});
+
+app.get('/generate_test', function(req, res){
+  var pdf = new pdfkit();
+
+  pdf.pipe(fs.createWriteStream('resume.pdf'));
+
+  pdf.font('Helvetica')
+    .fontSize(20)
+    .text('Ben has this email address: email@address.com wants to make a pdf with this text: text', 100, 100);
+
+  pdf.end();
+
+  res.send('<h1>Generating test resume!</h1>');
 });
 
 app.listen(PORT, function (){
@@ -30,6 +49,55 @@ app.listen(PORT, function (){
 });
 
 //Helper Functions
-function generatePDF(){
+function generatePDF(contents){
   var pdf = new pdfkit();
+
+  console.log(req.body.name);
+  console.log(req.body.example);
+  console.log(req.body.email);
+
+  //todo: randomize file name
+  pdf.pipe(fs.createWriteStream('resume.pdf'));
+
+  pdf.font('Helvetica')
+    .fontSize(20)
+    .text('${req.body.name} has this email address: ${req.body.email} wants to make a pdf with this text: ${req.body.example}', 100, 100);
+
+  pdf.end();
+}
+
+function mailPDF(path){
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+      host: 'smtp.1and1.com',
+      port: 587,
+      secure: true, // secure:true for port 465, secure:false for port 587
+      auth: {
+          user: 'resumegenerator@bankingandconsulting.com',
+          pass: 'Consultants8!'
+      }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Resume Generator" <resumegenerator@bankingandconsulting.com>', // sender address
+      to: 'ramankooner9@gmail.com', // list of receivers
+      subject: 'Hey Ram', // Subject line
+      text: 'Hello world ?', // plain text body
+      html: '<b>Hello world ?</b>', // html body
+      attachments: [
+        {
+          filename: 'resume.pdf',
+          path: 'resume.pdf'
+        }
+      ]
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+  });
 }
